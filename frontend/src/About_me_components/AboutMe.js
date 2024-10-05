a454cb1e2d9027cb238db598492387cd7d195fdd
Daniel <lewdaniel2@gmail.com>
2024-10-04 21:45:47 -0400

    created frontend folder for react

---
+++ frontend/src/About_me_components/AboutMe.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function AboutMe() {
+  return <h1>About Me</h1>;
+};
---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import './App.css';
+
+import { AboutMe } from "./About_me_components/AboutMe";
+
+function App() {
+  return (
+    <Fragment>
+        <AboutMe />
+      <p>Hello</p>
+    </Fragment>
+  );
+}
+
+export default App;
